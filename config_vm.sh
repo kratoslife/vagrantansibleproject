1776159e8fd7fe54ad0e5d2f67958ae96236304c
mateusz <pinkyoov@gmail.com>
2018-08-08 11:30:43 +0200

    Initial commit

---
+++ config_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sed -i 's/ONBOOT=no/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^\(Defaults.*requiretty\)/#\1/' /etc/sudoers
+chkconfig iptables off
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+mkdir -m 0700 -p /home/vagrant/.ssh
+mv /root/id_rsa.pub /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
